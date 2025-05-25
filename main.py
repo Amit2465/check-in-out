6439a5e61b5a5ac85c850e52d738bf526f86d8e3
Amit2465 <amityadav23461@gmail.com>
2025-05-26 01:29:28 +0530

    Changed delayed time

--- main.py
+++ main.py
@@ -3,35 +3,32 @@ import time
 import random
 from playwright.sync_api import sync_playwright
 import os
-import dotenv
-
-dotenv.load_dotenv()
 
 action = sys.argv[1]  
 
 
 delay_seconds = random.randint(0, 600)
-time.sleep(delay_seconds) 
+time.sleep(delay_seconds)
 
 with sync_playwright() as pw:
-    browser = pw.firefox.launch(headless=False)
+    browser = pw.firefox.launch(headless=True)  
     page = browser.new_page()
     page.goto("https://payroll.razorpay.com/login?redirect=%2Fdashboard")
 
-    page.get_by_placeholder("Enter your email address").fill(os.getenv("email"))
-    page.locator("#password").fill(os.getenv("password"))  
-    page.locator("#loginButton").click()  
+    page.get_by_placeholder("Enter your email address").fill(os.environ["email"])
+    page.locator("#password").fill(os.environ["password"])
+    page.locator("#loginButton").click()
 
-    page.wait_for_timeout(5000)     
+    page.wait_for_timeout(5000)
     page.get_by_text("Attendance").first.click()
     page.wait_for_timeout(5000)
 
     if action == "checkin":
-        checkin_button = page.get_by_role("button", name="Check In") 
+        checkin_button = page.get_by_role("button", name="Check In")
         if checkin_button.is_enabled() and checkin_button.is_visible():
             checkin_button.click()
     elif action == "checkout":
         page.get_by_role("button", name="Check Out").click()
 
-    page.wait_for_timeout(2000)     
+    page.wait_for_timeout(2000)
     browser.close()
